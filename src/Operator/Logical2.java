c219896a0de1954ead1762ad2b71f6ccd6eb0bf0
YUNMI LEE <goya3014@hanmail.net>
2024-10-18 19:09:26 +0900

    논리 연산자

---
+++ src/Operator/Logical2.java
@@ -0,0 +1,9 @@
+package Operator;
+
+public class Logical2 {
+    public static void main(String[] args) {
+        int a = 15;
+        boolean result = 10 < a && a < 20;
+        System.out.println("result = " + result);
+    }
+}
